e4fc86853e2f286d1757be1d1f69d67cbde8a5a5
Damian Moore <damian@epixstudios.co.uk>
2020-01-13 17:15:30 +0000

    Receiver remembers which mode (bass/mid/treb) it was listening to across power-downs. Resets radio if a crash occurs.

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,4 +1,5 @@
 #include <SPI.h>
+#include <EEPROM.h>
 #include <NRFLite.h>
 
 #define pinButton 2
@@ -10,8 +11,10 @@
 const static uint8_t RADIO_ID = 0;       // Our radio's id.  The transmitter will send to this id.
 const static uint8_t PIN_RADIO_CE = 9;
 const static uint8_t PIN_RADIO_CSN = 10;
+const int addr = 0;
 
 uint8_t mode = -1;
+uint32_t lastPacketTime = 0;
 
 
 struct RadioPacket // Any packet up to 32 bytes can be sent.
@@ -31,11 +34,47 @@ NRFLite _radio;
 RadioPacket _radioData;
 
 
-void setup() {   
+void initRadio() {
   //                                                      Bitrate                  Channel (0-125)
   if (!_radio.init(RADIO_ID, PIN_RADIO_CE, PIN_RADIO_CSN, NRFLite::BITRATE250KBPS, 27)) {
     while (1); // Wait here forever.
   }
+}
+
+
+void changeMode(bool firstRun) {
+  mode = EEPROM.read(addr);
+  if (!firstRun) {
+    mode += 1;
+  }
+  if (mode > 2) {
+    mode = 0;
+  }
+  EEPROM.write(addr, mode);
+
+  // Flash LEDs to show what channel (bass, mid, treb) they are displaying
+  for (int i = 0; i < mode + 1; i++) {
+    digitalWrite(pinLedRed, LOW);
+    digitalWrite(pinLedGreen, LOW);
+    digitalWrite(pinLedBlue, LOW);
+    delay(200);
+    digitalWrite(pinLedRed, HIGH);
+    digitalWrite(pinLedGreen, HIGH);
+    digitalWrite(pinLedBlue, HIGH);
+    delay(50);
+  }
+
+  digitalWrite(pinLedRed, LOW);
+  digitalWrite(pinLedGreen, LOW);
+  digitalWrite(pinLedBlue, LOW);
+  delay(500);
+
+  lastPacketTime = millis();
+}
+
+
+void setup() {
+  initRadio();
 
   pinMode(pinButton, INPUT_PULLUP);
   pinMode(pinLedRed, OUTPUT);
@@ -62,39 +101,16 @@ void setup() {
   digitalWrite(pinLedBlue, LOW);  
   delay(500);
 
-  changeMode();
-}
-
-
-void changeMode() {
-  mode += 1;
-  if (mode > 2) {
-    mode = 0;
-  }
-
-  for (int i = 0; i < mode + 1; i++) {
-    digitalWrite(pinLedRed, LOW);
-    digitalWrite(pinLedGreen, LOW);
-    digitalWrite(pinLedBlue, LOW);
-    delay(200);
-    digitalWrite(pinLedRed, HIGH);
-    digitalWrite(pinLedGreen, HIGH);
-    digitalWrite(pinLedBlue, HIGH);
-    delay(100);
-  }
-
-  digitalWrite(pinLedRed, LOW);
-  digitalWrite(pinLedGreen, LOW);
-  digitalWrite(pinLedBlue, LOW);
-  delay(500);
+  changeMode(true);
 }
 
 
 void loop() {
   if (digitalRead(pinButton) == LOW) {
-    changeMode();
+    changeMode(false);
   }
   while (_radio.hasData()) {
+    lastPacketTime = millis();
     _radio.readData(&_radioData); // Note how '&' must be placed in front of the variable name.
 
     if (mode == 0) {
@@ -112,5 +128,22 @@ void loop() {
       analogWrite(pinLedGreen, _radioData.TrebGreen);
       analogWrite(pinLedBlue, _radioData.TrebBlue);
     }
+  }
+
+  // Prevents LEDs getting stuck on if receiver stops receiving data (out of range, transmitter malfunction etc.)
+  if (millis() - lastPacketTime > 1000) {
+    digitalWrite(pinLedRed, LOW);
+    digitalWrite(pinLedGreen, LOW);
+    digitalWrite(pinLedBlue, LOW);
+  }
+
+  if (millis() - lastPacketTime > 10000) {
+    digitalWrite(pinLedRed, HIGH);
+    digitalWrite(pinLedGreen, LOW);
+    digitalWrite(pinLedBlue, LOW);
+    delay(50);
+    digitalWrite(pinLedRed, LOW);
+    _radio.powerDown();
+    initRadio();
   }
 }
